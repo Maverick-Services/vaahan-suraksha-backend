292e6b324cf6ee058a7061781590c6eff99c3305
Maverick-Services <maverickit123@gmail.com>
2025-09-22 13:06:02 +0530

    Cors removed from app

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ import { ApiError } from "./utils/ApiError.js"
 const app = express()
 
 app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
+    // {
+    //     origin: process.env.CORS_ORIGIN,
+    //     credentials: true
+    // }
 ))
 
 // app.use(express.json({ limit: "16kb" }))
